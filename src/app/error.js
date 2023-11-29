ad00075c1fa59a2b249d42101e2616dd91dc5f88
Aditya <adityaaroraaditya12@gmail.com>
2023-11-29 20:26:02 +0530

    updated

--- src/app/error.js
+++ src/app/error.js
@@ -9,16 +9,10 @@ export default function Error({ error, reset }) {
   }, [error]);
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+    <div className="flex flex-col items-center h-full gap-7 justify-center min-h-screen p-5 bg-gray-100 min-w-screen">
+      <h2 className="text-2xl text-red-600">404 Not Found</h2>
+      <p>something went wrong</p>
+      <Button onClick={() => reset()}>try again</Button>
     </div>
   );
 }
