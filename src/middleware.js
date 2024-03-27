51dd388d6fb2f3e01d297beb32ed8b0a2d00987f
Adityasgit <129075017+Adityasgit@users.noreply.github.com>
2024-03-27 21:32:04 +0530

    skipped auth

--- src/middleware.js
+++ src/middleware.js
@@ -1,28 +1,28 @@
-import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
-import { NextResponse } from "next/server";
+// import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+// import { NextResponse } from "next/server";
 
-export async function middleware(req) {
-  const res = NextResponse.next();
-  const supabase = createMiddlewareClient({ req, res });
+// export async function middleware(req) {
+//   const res = NextResponse.next();
+//   const supabase = createMiddlewareClient({ req, res });
 
-  const data = await supabase.auth.getUser();
-  // if user is signed in and the current path is / redirect the user to /account
-  if (
-    data.data.user &&
-    (req.nextUrl.pathname === "/login" ||
-      req.nextUrl.pathname === "/" ||
-      req.nextUrl.pathname === "/register")
-  ) {
-    return NextResponse.redirect(new URL("/chat", req.url));
-  }
+//   const data = await supabase.auth.getUser();
+//   // if user is signed in and the current path is / redirect the user to /account
+//   if (
+//     data.data.user &&
+//     (req.nextUrl.pathname === "/login" ||
+//       req.nextUrl.pathname === "/" ||
+//       req.nextUrl.pathname === "/register")
+//   ) {
+//     return NextResponse.redirect(new URL("/chat", req.url));
+//   }
 
-  if (!data.data.user && req.nextUrl.pathname.startsWith("/chat")) {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
+//   if (!data.data.user && req.nextUrl.pathname.startsWith("/chat")) {
+//     return NextResponse.redirect(new URL("/", req.url));
+//   }
 
-  return res;
-}
+//   return res;
+// }
 
-// export const config = {
-//   matcher: ["/", "/chat", "/login", "/register"],
-// };
+// // export const config = {
+// //   matcher: ["/", "/chat", "/login", "/register"],
+// // };
